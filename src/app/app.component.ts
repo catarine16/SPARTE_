045e242d49c6edc194f592997a7e1aad4914744d
emymoreira <emilymoreira9090@gmail.com>
2024-04-18 20:28:36 -0300

    fundo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet, RouterModule } from '@angular/router';
 import { TopbarComponent } from './pages/topbar/topbar.component';
 import { CommonModule } from '@angular/common';
-
+ 
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -12,4 +12,16 @@ import { CommonModule } from '@angular/common';
 })
 export class AppComponent {
   title = 'Sparte';
+  ngOnInit(): void {
+    const img: HTMLImageElement = document.createElement("img");
+    img.src = "/assets/img/background.png";
+    img.style.position = "fixed";
+    img.style.top = "0";
+    img.style.left = "0";
+    img.style.width = "100vw";
+    img.style.height = "100vh";
+    img.style.zIndex = "-1";
+    document.body.appendChild(img);
+  }
+ 
 }
