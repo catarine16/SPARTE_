1dee7817140d3d7e25e0b7cbb68477e2ed97c20e
emymoreira <emilymoreira9090@gmail.com>
2024-05-02 16:14:02 -0300

    mudanças no topbar (com bug no close button)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,7 +14,7 @@ export class AppComponent {
   title = 'Sparte';
   ngOnInit(): void {
     const img: HTMLImageElement = document.createElement("img");
-    img.src = "/assets/img/background.png";
+    img.src = "/assets/img/back2.png";
     img.style.position = "fixed";
     img.style.top = "0";
     img.style.left = "0";
