85ae25b1f737bf0bb3ec2cba0575cf602853f220
Sora018 <renan.mc.2005.2014@gmail.com>
2024-05-11 21:44:59 -0300

    configurações do firebase e adicionamento da parte de upload

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,34 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
+
+import { provideFirebaseApp, getApp, initializeApp } from '@angular/fire/app';
+import { getStorage, provideStorage } from '@angular/fire/storage';
+import { provideAuth, getAuth } from '@angular/fire/auth';
 
 import { routes } from './app.routes';
 
+const firebaseConfig = {
+  apiKey: "AIzaSyD0iKGNhTflF_lNhbDnPcXFOymMk2iw4uA",
+  authDomain: "sparte-angular-teste.firebaseapp.com",
+  projectId: "sparte-angular-teste",
+  storageBucket: "sparte-angular-teste.appspot.com",
+  messagingSenderId: "512410958567",
+  appId: "1:512410958567:web:e5f9d3ff14d69bbc8dc042"
+}; //FIREBASE PROJETO TESTE (caso precise testar algo pode mudar o que está entre {} para o do seu projeto.)
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    importProvidersFrom([
+    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideStorage(() => getStorage()),
+    provideAuth(() => getAuth()),
+  ]),
+], 
 };
+
+
+
+
+
+
---
+++ src/app/pages/upload/a.js
@@ -0,0 +1,70 @@
+const storage = firebase.storage();
+
+
+function voltar() {
+    window.location.href = "../pages/home/home.html";
+}
+
+
+const inp = document.querySelector(".inp");
+const progressbar = document.querySelector(".progress");
+const img = document.querySelector(".img");
+const fileData = document.querySelector(".filedata");
+const loading = document.querySelector(".loading");
+let file;
+let fileName;
+let progress;
+let isLoading = false;
+let uploadedFileName;
+
+const selectImage = () => {
+  inp.click();
+};
+
+const getImageData = (e) => {
+  file = e.target.files[0];
+  fileName = Math.round(Math.random() * 9999) + file.name;
+  if (fileName) {
+    fileData.style.display = "block";
+  }
+  fileData.innerHTML = fileName;
+  console.log(file, fileName);
+};
+
+const uploadImage = () => {
+  loading.style.display = "block";
+  const storageRef = storage.ref().child("myimages");
+  const folderRef = storageRef.child(fileName);
+  const uploadtask = folderRef.put(file);
+  uploadtask.on(
+    "state_changed",
+    (snapshot) => {
+      console.log("Snapshot", snapshot.ref.name);
+      progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+      progress = Math.round(progress);
+      progressbar.style.width = progress + "%";
+      progressbar.innerHTML = progress + "%";
+      uploadedFileName = snapshot.ref.name;
+    },
+    (error) => {
+      console.log(error);
+    },
+    () => {
+      storage
+        .ref("myimages")
+        .child(uploadedFileName)
+        .getDownloadURL()
+        .then((url) => {
+          console.log("URL", url);
+          if (!url) {
+            img.style.display = "none";
+          } else {
+            img.style.display = "block";
+            loading.style.display = "none";
+          }
+          img.setAttribute("src", url);
+        });
+      console.log("File Uploaded Successfully");
+    }
+  );
+};
