d570525f7cc503e0ddf4b500d5ae9937d5201c35
emymoreira <emilymoreira9090@gmail.com>
2024-06-16 23:13:30 -0300

    inspire-se finalizado:)

--- src/app/pages/inspiracao/inspiracao.component.ts
+++ src/app/pages/inspiracao/inspiracao.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-inspiracao',
-  standalone: true,
-  imports: [],
-  templateUrl: './inspiracao.component.html',
-  styleUrl: './inspiracao.component.css'
+selector: 'app-inspiracao',
+standalone: true,
+imports: [],
+templateUrl: './inspiracao.component.html',
+styleUrl: './inspiracao.component.css'
 })
 export class InspiracaoComponent {
 
